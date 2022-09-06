edec11474bc118c3bb2341231346362cc787fb7f
MartinStokholmWinTerm <msldk6a@gmail.com>
2022-09-06 20:44:27 +0200

    Project with grid, added shortcut eventhandler for scaling up/down of font. Click event on image displays name of boat

--- Lesson3Sailboat/SailboatUI_Grid/MainWindow.xaml.cs
+++ Lesson3Sailboat/SailboatUI_Grid/MainWindow.xaml.cs
@@ -26,6 +26,36 @@ namespace SailboatUI_Grid
             InitializeComponent();
             sailboat = new Sailboat();
             Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            PreviewKeyDown += new KeyEventHandler(MainWindow_PreviewKeyDown);
+        }
+
+        private void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.L:
+                    {
+                        if (Keyboard.Modifiers == ModifierKeys.Control & FontSize < 42)
+                        {
+                            FontSize += 2;
+                            e.Handled = true;
+                        }
+                    }
+                    break;
+
+                case Key.S:
+                    {
+                        if (Keyboard.Modifiers == ModifierKeys.Control & FontSize > 12)
+                        {
+                            FontSize -= 2;
+                            e.Handled = true;
+                        }
+                    }
+                    break;                
+
+                default:
+                    break;
+            }
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
@@ -35,7 +65,19 @@ namespace SailboatUI_Grid
 
         private void tbxLength_TextChanged(object sender, TextChangedEventArgs e)
         {
-            sailboat.Length = Double.Parse(tbxLength.Text);
+       
+            if (tbxLength.Text.Trim() != "")
+            {
+                try
+                {
+                    sailboat.Length = Double.Parse(tbxLength.Text);
+                }
+                catch 
+                {
+                    MessageBox.Show("Length can only be expressed using numbers");
+                    tbxLength.Text = "";
+                }
+            }
         }
 
         private void tbxName_TextChanged(object sender, TextChangedEventArgs e)
@@ -46,6 +88,15 @@ namespace SailboatUI_Grid
         private void btnCalculateHullSpeed_Click(object sender, RoutedEventArgs e)
         {
             tbxHullSpeed.Text = sailboat.Hullspeed().ToString("F1");
+            
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (tbxName.Text.Trim() != "")
+                MessageBox.Show($"This boat is named {sailboat.Name}");
+            else         
+                MessageBox.Show("This boat aint got a name yet!");
             
         }
     }
